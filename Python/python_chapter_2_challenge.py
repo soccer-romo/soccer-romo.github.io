84dd38494df3cba7d83e94701d239cae55c284f3
David <dn0885594@otc.edu>
2019-02-06 22:29:49 -0600

    python

---
+++ Python/python_chapter_2_challenge.py
@@ -0,0 +1,13 @@
+restarant_bill_total = 0
+tip_15 = .15
+tip_20 = .20
+tip_0 = 0
+restarant_bill_total = int(input("How much is your restarant bill today? "))
+print (restarant_bill_total)
+
+tip_0 = input ("How much would you like to tip today 15% or 20%? ")
+if tip_0:  15
+tip_0 * restarant_bill_total
+if tip_0:  20
+tip_0 * restarant_bill_total
+print (restarant_bill_total*tip_0)
