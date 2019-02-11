aabdf6d5a1b641555ebb4786375743ac2d388f3d
David <dn0885594@otc.edu>
2019-02-11 12:40:19 -0600

    changes

--- Python/Tipper_2.0.py
+++ Python/Tipper_2.0.py
@@ -1,7 +1,7 @@
 bill_total = 0 
 tip_percent = 0
 
-bill_total = int(input("How much was your bill today? $"))
+bill_total = float(input("How much was your bill today? $"))
 
 tip_percent= int(input("How much would you like to tip today 15% or 20%? "))
 
@@ -9,6 +9,6 @@ tip_percent = tip_percent * .01
 
 bill_total = bill_total * tip_percent + bill_total
 
-print("Okay so your total plus tip will be: $" + str(bill_total))
+print("Your total plus tip will be: $" + str(bill_total))
 
 input("\n\nPress the enter key to exit")
