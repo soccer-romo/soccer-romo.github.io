cd1342e9a82ae5c4a8c053d7dadfd99fecb8ad4c
David <soccer-romo@users.noreply.github.com>
2019-02-27 21:20:08 -0600

    py up

--- Python/Chap_3_Chall.3.py
+++ Python/Chap_3_Chall.3.py
@@ -11,27 +11,29 @@ chances = 2
 user_answer = "yes"
 
 while (user_answer == "yes" or user_answer == "y"):
-	my_number = str(random.randrange(0,11))
-	chances = 2
-	user_variable = input("Guess my number between 0 and 10. You get three chances: ")
-	
-	if (user_variable == my_number):
-		print("Good Job! You've guessed my number")
-	while (user_variable != my_number):
-		user_variable = input("Too bad! Try again! ")
-		chances -= 1
-		if(user_variable == my_number):
-			print("Good Job! You've guessed my number!")
-			break
-		if (chances == 0):
-			print("Game Over. My number was " + str(my_number))
-			break
+        my_number = str(random.randrange(0,11))
+        chances = 2
+        user_variable = input("Guess my number between 0 and 10. You get three chances: ")
+
+        while(int(user_variable) >= 0 or int(user_variable )<= 10):
+                
+                if (user_variable == my_number):
+                        print("Good Job! You've guessed my number")
+                while (user_variable != my_number):
+                        user_variable = input("Too bad! Try again! ")
+                        chances -= 1
+                        if(user_variable == my_number):
+                                print("Good Job! You've guessed my number!")
+                                break
+                        if (chances == 0):
+                                print("Game Over. My number was " + str(my_number))
+                                break
 	
 
-	user_answer = input("Do you want to play again? ").lower()
+        user_answer = input("Do you want to play again? ").lower()
 
-	while(user_answer != "yes" and user_answer != "y" and user_answer != "no" and user_answer != "n"):
-		user_answer = input("Please enter in yes or no: ")
-	if (user_answer == "no" or user_answer == "n"):
-		print("Thank you for playing!")
-		break
+        while(user_answer != "yes" and user_answer != "y" and user_answer != "no" and user_answer != "n"):
+                user_answer = input("Please enter in yes or no: ")
+        if (user_answer == "no" or user_answer == "n"):
+                print("Thank you for playing!")
+                break
