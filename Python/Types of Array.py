64414052da2c4b6e5510f2ead60241283b45cbc9
David <dn0885594@otc.edu>
2019-03-18 08:03:20 -0500

    Create Types of Array.py

---
+++ Python/Types of Array.py
@@ -0,0 +1,38 @@
+#array
+
+#tuple
+'''
+import random
+colors = ("red","green","blue")
+
+print(colors[random.randint(0,2)])
+
+
+for color in colors:
+	print(color)
+'''
+#list
+'''
+colors = ["red","green","blue"]
+
+colors[2] = "grey"
+
+print(colors)
+'''
+
+#nested list
+#scores = [("moe",1000),("curely",1200),("larry",1500)]
+'''
+scores = [["moe",1000],["curely",1200],["larry",1500]]
+print(scores[2][0])
+scores [0][1] = 1300
+print(scores)
+'''
+#dictionary
+
+score = {"moe":[1000, "springfield", "north"],"curley":[1200, "springfield", "south"],"larry":[1500, "springfield", "north"]}
+
+#print(score["curley"])
+
+for k,v in score.items():
+	print(k,v[0],v[1])
