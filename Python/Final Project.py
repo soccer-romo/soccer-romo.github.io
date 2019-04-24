71f071fcd05cf5a7937078441213619844e66e4b
David <dn0885594@otc.edu>
2019-04-24 18:26:05 -0500

    creating final project

---
+++ Python/Final Project.py
@@ -0,0 +1,12 @@
+#pusdocode
+#create module
+#create class and define functions
+#-----------------------------------
+#functions needed
+# 1) Have three arrays
+#	a)engine type
+#	b)vehicle type
+#	c)color avalible
+# 2) Have each vhicle type have it's own set of engines and color types
+# 3) have user picking out what he/she wants
+# 4) Spits out price for car
--- Python/Game_of_war.py
+++ Python/Game_of_war.py
@@ -13,11 +13,80 @@
 # deal the cards
 # play the games
 
+import Card_module
+import math
+
+class War_Card(Card_module.Card):
+	"""A War Card, Aces High"""
+	@property
+	def value(self):
+		if self.is_face_up:
+			if self.ranks == "J":
+				v = 11
+			elif self.ranks == "Q":
+				v = 12
+			elif self.ranks == "K":
+				v = 13
+			elif self.ranks == "A":
+				v = 14
+			else:
+				v = War_Card.RANKS.index(self.ranks) + 1
+		else:
+			v = None
+		return v
+
+class War_Deck(Card_module.Deck):
+	"""A War Deck"""
+	def populate(self):
+			for suit in War_Card.SUITS:
+				for rank in War_Card.RANKS:
+					self.add(War_Card(rank,suit))
+
 def create_players(numbersOfPlayers = 2):
 	players = []
 	for person in range(numbersOfPlayers):
-		players.append("Players" + str(person))
+#		players.append("Players" + str(person))
+		player = Card_module.Hand()
+		players.append(player)
 	return players
+
+
+deck = War_Deck()
+#print (deck)
+deck.populate()
+#print(deck)
+deck.shuffle()
+#print(deck)
+
+players = create_players()
+#print(players)
+cardsPerPlayer = math.trunc(52 / len(players))
+deck.deal(players, per_hand = cardsPerPlayer)
+
+for player in players:
+	for card in player.cards:
+		print("Player", players.index(player))
+		print(card.value)
+
+
+
+
+
+
+
+
+
+"""
+card1 = War_Card("A", "s")
+print(card1)
+print(card1.value)
+
+
+
+card2 = War_Card("3", "c")
+print(card2)
+print(card2.value)
+
 	
 def build_deck():
 	deck = {}
@@ -26,12 +95,12 @@ def build_deck():
 		for n in range(13):
 			deck[suite + str(n+1)] = n+1
 	return deck
-#def deal(deck, players):
-		
+def deal(deck, players):
+	
 	
 
 players = create_players()
 print(players)
 deck = build_deck()
 print(deck)
-
+"""
