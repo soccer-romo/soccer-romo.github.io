e857a7f2b2c625663454a3d348b3f13058864d67
David <dn0885594@otc.edu>
2019-10-12 01:01:42 -0500

    version 1

--- WebDevII/AddressBookProjec/script.js
+++ WebDevII/AddressBookProjec/script.js
@@ -1,30 +1,40 @@
-
+contacts = document.getElementById('contacts')
 
 function AddressBook(firstN,lastN,phoneNum,email,src)
 {
-    var contactImg = document.getElementById('contactImg')
-    var contactFirstName = document.getElementById('FirstP')
-    var contactLastName = document.getElementById('LastP')
-    var contactEmail = document.getElementById('EmailP')
-    var contactPhone = document.getElementById('PhoneP')
+    
     this.firstN = firstN;
     this.lastN = lastN;
     this.phoneNum = phoneNum;
     this.email = email;
-    this.imgSrc = src
+    this.imgSrc = src;
     this.DisplayInfo = function()
     {
-        contactImg.src = this.imgSrc;
-        contactFirstName.innerHTML = this.firstN;
-        contactLastName.innerHTML = this.lastN;
-        contactEmail.innerHTML = this.email;
-        contactPhone.innerHTML = this.phoneNum;
+        return `Name: ${this.firstN} ${this.lastN}\n
+        Phone: ${this.phoneNum}
+        Email: ${this.email}`
     }
     this.DisplayName = function()
     {
-        return `${this.firstN} ${this.lastN}`
+        return `${this.firstN} ${this.lastN}\n`
     }
 }
 
-const contact1 = new AddressBook("David","Niculcea","417-379-5743","davsocc@gmail.com","images/")
-contact1.DisplayInfo()
+const contact1 = new AddressBook("David","Niculcea","417-379-5743","davsocc@gmail.com","images/david.")
+const contact2 = new AddressBook("Marius","Lupescu","417-861-4650","marius@yahoo.com","images/marius.")
+const contact3 = new AddressBook("Ruben","Niculcea","441-234-7643","Ruben@gmail.com","images/ruben.")
+const contact4 = new AddressBook("Jessica", "Niculcea", "574-285-4957","jess@AOL.net","images/jessica.")
+
+contactAry = [contact1,contact2,contact3,contact4];
+
+var contactList = "";
+for(var i = 0; i < contactAry.length; i ++)
+{
+    contactList += `<li id = ${i}> ${contactAry[i].DisplayName()} </li>`
+}
+contacts.innerHTML = contactList;
+
+document.getElementById("contacts").addEventListener('click',function(e)
+{
+    document.getElementById('contactInfo').innerHTML = contactAry[e.target.id].DisplayInfo();
+})
